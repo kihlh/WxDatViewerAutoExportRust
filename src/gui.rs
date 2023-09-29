0044b327bff50b9b087e59ac2345aa835facadfe
kihlh <kihlh@qq.com>
2023-09-29 11:43:26 +0800

    更新授权协议

--- src/gui.rs
+++ src/gui.rs
@@ -514,7 +514,7 @@ pub fn mianWindow(show: bool) -> Result<MianWindowItme> {
     // input_Console.buff.append("\n");
     // input_Console
     //     .buff
-    //     .append("软件开源协议 Mit 2.0  (但是并不包含解码算法)  版本：1.0.0 ");
+    //     .append("软件开源协议 GPL 3.0  (但是并不包含解码算法)  版本：1.0.0 ");
     // input_Console.buff.append("\n\n");
     // input_Console
     //     .buff
